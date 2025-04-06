9da4a03209794433fecf40ee0a76c4757e6417d5
rachel157 <hungphamm1507@gmail.com>
2025-04-07 00:40:40 +0700

    JWT Token

--- src/main/java/com/devteria/identityservice/controller/AuthenticationController.java
+++ src/main/java/com/devteria/identityservice/controller/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
 public class AuthenticationController {
